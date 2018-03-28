63ad56095cf334c9f7eeaeb3692df9350251f213
Александр <ca.dev@datalink.ua>
2018-03-28 20:28:22 +0300

    Initial Commit

---
+++ Chat quickblox/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Chat quickblox
+//
+//  Created by Admin on 28.03.18.
+//  Copyright © 2018 Tsvigun Aleksander. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
